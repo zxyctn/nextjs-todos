d43804a63a0fe8bd1ddf27f37030425b5ad02525
Ibrahim Mammadov <zxyctn@gmail.com>
2024-09-06 13:59:49 +0200

    Export full types

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,36 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 let prisma: PrismaClient;
+
+export type TaskWithActivities = Prisma.TaskGetPayload<{
+  include: {
+    activities: true;
+  };
+}>;
+
+export type GroupWithTasks = Prisma.GroupGetPayload<{
+  include: {
+    tasks: {
+      include: {
+        activities: true;
+      };
+    };
+  };
+}>;
+
+export type WorkspaceWithGroups = Prisma.WorkspaceGetPayload<{
+  include: {
+    groups: {
+      include: {
+        tasks: {
+          include: {
+            activities: true;
+          };
+        };
+      };
+    };
+  };
+}>;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
